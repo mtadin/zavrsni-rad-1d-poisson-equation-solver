b3ac67bfa5a91aec588c0c692fa8959a090e4158
Marin Tadin <51207820+mtadin@users.noreply.github.com>
2021-02-01 17:17:37 +0100

    added phi0 and phiL to model

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from tensorflow.keras import layers
 from tensorflow.keras import models
 
 # dataset
-dataset = pd.read_csv('dataset.csv', sep=',', names=['x', 'rho_eps', 'Labels'])
+dataset = pd.read_csv('dataset.csv', sep=',', names=['x', 'phi0', 'phiL', 'rho_eps', 'Labels'])
 inputs = dataset.copy()
-inputs['rho_eps'] = inputs['rho_eps']/1000 # normalizacija inputa
+
+# normalizacija inputa
+inputs['rho_eps'] = inputs['rho_eps']/1000
+inputs['phi0'] = inputs['phi0']/1000
+inputs['phiL'] = inputs['phiL']/1000
+
 outputs = inputs.pop('Labels')
 # print(inputs)
 # print(outputs)
@@ -27,7 +32,7 @@ adam = tf.keras.optimizers.Adam(epsilon=1.0) # default value: 1e-7
 model.compile(loss='mse', optimizer=adam) # optimizer adam ili SGD
 
 # treniranje modela
-model.fit(inputs.values, outputs.values, epochs=200)
+model.fit(inputs.values, outputs.values, epochs=250)
 
 # spremanje modela
 modelName = input("Unesite ime modela za spremanje: ")
--- plot.py
+++ plot.py
@@ -15,6 +15,9 @@ while True:
 modelName = input("> Unesite naziv modela kojim zelite vrsiti usporedbu: ")
 modelName = modelName + '.h5'
 
+phi0 = 100
+phiL = 0
+
 x = np.arange(0, 1, 0.01)
 # stvaranje filea za usporedbu
 with open('test.csv', 'w') as csvfile:
@@ -25,7 +28,7 @@ with open('test.csv', 'w') as csvfile:
         )
 
     for _x in x:             
-        filewriter.writerow([_x, inputNumber])
+        filewriter.writerow([_x, phi0, phiL, inputNumber])
 
 # ucitavanje spremljenog modela
 try:
@@ -37,8 +40,12 @@ except:
     print("> Model ne postoji!")
 
 # usporedba grafova
-test = pd.read_csv('test.csv', sep=',', names=['x', 'y'])
-test['y'] = test['y']/1000 # normalizacija inputa
+test = pd.read_csv('test.csv', sep=',', names=['x', 'phi0', 'phiL', 'y'])
+
+# normalizacija inputa
+test['y'] = test['y']/1000
+test['phi0'] = test['phi0']/1000
+test['phiL'] = test['phiL']/1000
 
 predictNew = model.predict(test)
 
@@ -46,8 +53,6 @@ def phi(x, L, phi0, phiL, rho_eps):
     phi = -0.5 * rho_eps * np.power(x, 2) + ((phiL - phi0)/L + 0.5 * rho_eps * L) * x + phi0
     return phi
 
-phi0 = 100
-phiL = 0
 L = 1
 dx = L / 100
 x = np.arange(0, L, dx)
