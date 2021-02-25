010cef5167f2e25666e8e7bbb24a3e8fe92df379
Marin Tadin <51207820+mtadin@users.noreply.github.com>
2021-02-25 17:55:30 +0100

    attempt to improve accuracy at lower values of phi0 and phiL

--- dataset-generator.py
+++ dataset-generator.py
@@ -19,11 +19,35 @@ with open('dataset.csv', 'w') as csvfile:
         lineterminator='\n'
         )
 
-    for step1 in range(-1000, 1001, 40):
-        phi0 = step1
-        for step2 in range(-1000, 1001, 40):
-            phiL = step2
+    # for step1 in range(-1000, 1001, step):
+    #     phi0 = step1
+    #     for step2 in range(-1000, 1001, step):
+    #         phiL = step2
+    #         for num in rho_eps:
+    #             for _x in x:
+    #             # print(num, phi(num))               
+    #                 filewriter.writerow([_x, num, phi0, phiL, phi(_x, L, phi0, phiL, num)])
+
+    step = 80
+    start0 = -1000
+    stop0 = 1001
+    startL = -1000
+    stopL = 1001
+    while start0 < stop0:
+        phi0 = start0
+        startL = -1000 # potrebno je resetirati unutarnji loop inace generacija stane nakon prvog izvodenja
+        while startL < stopL:
+            phiL = startL
             for num in rho_eps:
-                for _x in x:
-                # print(num, phi(num))               
+                for _x in x:           
                     filewriter.writerow([_x, num, phi0, phiL, phi(_x, L, phi0, phiL, num)])
+            if (-200 < startL < 99):
+                startL += 5
+            else:
+                startL += 100
+        if (-200 < start0 < 99):
+            start0 += 5
+        else:
+            start0 += 100
+
+    
